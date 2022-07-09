f6428eadbfa43a72fe1a8cc879e15259da7fa081
unknown <ponvannanstatk@gmail.com>
2022-07-09 23:57:04 +0400

    first commit

---
+++ API/Data/Migrations/20220709113301_ModifiedProdcutColumnname.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class ModifiedProdcutColumnname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityStock",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityStock");
+        }
+    }
+}
